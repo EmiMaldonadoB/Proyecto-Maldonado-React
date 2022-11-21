d2fa87f8da6006ad472ff80e75ec245424d01622
EmiMaldonadoB <109559421+EmiMaldonadoB@users.noreply.github.com>
2022-11-21 20:09:57 -0300

    -

---
+++ components/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+const ItemListContainer = (props) => {
+    return (
+        <div><h2>asjdasidjas</h2></div>
+    )
+}
+
+export default ItemListContainer;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+
+const App = () => {
+  return (
+    <>
+      
+      <Navbar/>
+      <ItemListContainer mensaje="Hola ItemListContainer"/>
+    </>
+  )
+}
+
+export default App;
